70e512531eb411daee369c7aab716be5f08fa79b
Novikov Roman <service.for.roman@gmail.com>
2020-03-12 17:19:38 +0300

    fix: added platforms: console, tournament

--- platform.go
+++ platform.go
@@ -8,18 +8,32 @@ func (p Platform) String() string {
 	return p.code
 }
 
+// Steam
 func SteamPlatform() Platform {
 	return Platform{code: "steam"}
 }
 
+// PS4
 func PsnPlatform() Platform {
 	return Platform{code: "psn"}
 }
 
+// Xbox
 func XboxPlatform() Platform {
 	return Platform{code: "xbox"}
 }
 
+// Kakao
 func KakaoPlatform() Platform {
 	return Platform{code: "kakao"}
+}
+
+// PS4/Xbox (used for the /matches and /samples endpoints)
+func ConsolePlatform() Platform {
+	return Platform{code: "console"}
+}
+
+// Tournaments
+func TournamentPlatform() Platform {
+	return Platform{code: "tournament"}
 }
