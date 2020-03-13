176559668834880b0c0859e9bed4ba70f3a087a8
Novikov Roman <service.for.roman@gmail.com>
2020-03-13 11:52:27 +0300

    fix: platform transform and validation added

--- platform.go
+++ platform.go
@@ -1,5 +1,7 @@
 package pubg
 
+import "errors"
+
 const (
 	EmptyPlatform      = ""
 	SteamPlatform      = "steam"
@@ -14,4 +16,45 @@ type Platform string
 
 func (p Platform) IsEmpty() bool {
 	return p == EmptyPlatform
+}
+
+func (p Platform) IsValid() bool {
+	return p == EmptyPlatform || p == SteamPlatform || p == PsnPlatform || p == XboxPlatform ||
+		p == KakaoPlatform || p == ConsolePlatform || p == TournamentPlatform
+}
+
+func TransformToPlatform(name string) (platform Platform, err error) {
+	platform = EmptyPlatform
+
+	switch name {
+	case EmptyPlatform:
+		return
+
+	case SteamPlatform:
+		platform = SteamPlatform
+		return
+
+	case PsnPlatform:
+		platform = PsnPlatform
+		return
+
+	case XboxPlatform:
+		platform = XboxPlatform
+		return
+
+	case KakaoPlatform:
+		platform = KakaoPlatform
+		return
+
+	case ConsolePlatform:
+		platform = ConsolePlatform
+		return
+
+	case TournamentPlatform:
+		platform = TournamentPlatform
+		return
+	}
+
+	err = errors.New("Platform not found. ")
+	return
 }
--- platform_test.go
+++ platform_test.go
@@ -6,32 +6,72 @@ import (
 )
 
 func TestClient_Platform(t *testing.T) {
-	var p Platform
+	var (
+		p   Platform
+		err error
+	)
+
 	p = EmptyPlatform
 	require.True(t, p.IsEmpty())
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), EmptyPlatform)
+	p, err = TransformToPlatform(EmptyPlatform)
+	require.Nil(t, err)
 	require.Equal(t, string(p), EmptyPlatform)
 
 	p = SteamPlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "steam")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), SteamPlatform)
+	p, err = TransformToPlatform(SteamPlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), SteamPlatform)
 
 	p = PsnPlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "psn")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), PsnPlatform)
+	p, err = TransformToPlatform(PsnPlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), PsnPlatform)
 
 	p = XboxPlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "xbox")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), XboxPlatform)
+	p, err = TransformToPlatform(XboxPlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), XboxPlatform)
 
 	p = KakaoPlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "kakao")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), KakaoPlatform)
+	p, err = TransformToPlatform(KakaoPlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), KakaoPlatform)
 
 	p = ConsolePlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "console")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), ConsolePlatform)
+	p, err = TransformToPlatform(ConsolePlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), ConsolePlatform)
 
 	p = TournamentPlatform
 	require.False(t, p.IsEmpty())
-	require.Equal(t, string(p), "tournament")
+	require.True(t, p.IsValid())
+	require.Equal(t, string(p), TournamentPlatform)
+	p, err = TransformToPlatform(TournamentPlatform)
+	require.Nil(t, err)
+	require.Equal(t, string(p), TournamentPlatform)
+
+	p = "unknown"
+	require.False(t, p.IsEmpty())
+	require.False(t, p.IsValid())
+	require.Equal(t, string(p), "unknown")
+	p, err = TransformToPlatform(string(p))
+	require.NotNil(t, err)
+	require.Equal(t, string(p), EmptyPlatform)
 }
